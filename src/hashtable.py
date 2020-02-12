2ef7bd5dffdfcff30f917d57a8c95afa3f675517
karenjli <karenjy.li@gmail.com>
2020-02-11 21:59:19 -0600

    MVP completed

--- src/hashtable.py
+++ src/hashtable.py
@@ -54,51 +54,80 @@ class HashTable:
         # Fill this in.
         # '''
         # Define index as a hashed key
-        index = self._hash(key)
-        # Point to the location of the specified index
-        node = self.storage[index]
+
+        index = self._hash_mod(key)
+
         # Check if the node exist
-        if node is None:
+        if self.storage[index] is None:
             # Just assigned the index above to a newly created Linked Pair
             self.storage[index] = LinkedPair(key, value)
-
         # if node is not None ==> Collision situation
+        else:
+            # Save the current value that is being collided
+            current = self.storage[index]
+            # Check and make sure that value is not none
+            while current is not None:
+                if current.key == key:
+                    # Rewrite the value if key is the same
+                    current.value = value
+                    break
+                else:  # The key is different
+                    # Check if next is empty
+                    if current.next is None:
+                        # Add a value behind if the key is different
+                        # By Adding a new pair
+                        current.next = LinkedPair(key, value)
+                    else:
+                        # Reassign the value is next is not None
+                        current = current.next
+
+        # Point to the location
 
     def remove(self, key):
-        # '''
-        # Remove the value stored with the given key.
+        # Get index
+        index = self._hash_mod(key)
+        # Point to the current key, value pair as indicated by index
+        current = self.storage[index]
+        # If current has nothing
+        if current is None:
+            # Returns message
+            return "No key found"
+        # Else check if current.next and the key matches
+        elif current.next is None and current.key == key:
+            # Reset the value to None
+            self.storage[index] = None
+        else:  # Either there is a next node after current or the key doesn't match
+            # set previous node to None
+            prevNode = None
+            # set the next node to what's after current
+            nextNode = current.next
 
-        # Print a warning if the key is not found.
+            while current is not None:
+                # If the key matches
+                if current.key == key:
 
-        # Fill this in.
-        # '''
-        # Look for the index
-        index = self._hash(key)
-        # Point to the location
-        node = self.storage[index]
-        # Check to see the node actually exists
-        while node is not None and node.key != key:
-            # Define Prev as Node??
-            prev = node
-            # Overwrite the node to be deleted with its next one
-            node = node.next
-        # Goes through the whole loop and found nothing
-        if node is None:
-            return None
-        # That means something was found
-        else:
-            # Define result
-            result = node.value
-            # Same procedure as removing from Linked List
-            if prev is None:
-                node = None
-            else:
-                # prev.next is basically node, which now equals to node.next
-                # which is the value after original node
-                prev.next = prev.next.next
-            # since result = node.value, and node is overwritten
-            # result now has the new value
-            return result
+                    if prevNode is None:
+                        # reset the index to the next node
+                        self.storage[index] = nextNode
+                    else:
+                        # this is needed to not delete previous value
+                        if nextNode:
+                            # reset the next value of the prevNode
+                            prevNode.next = nextNode
+                        else:
+                            # nextNode would be None
+                            prevNode.next = None
+                    # self.count -= 1
+                    break
+                else:
+                    if current.next is None:
+                        return "No key found"
+                    else:
+                        # Overwrite current value
+                        prevNode = current
+                        current = current.next
+                        nextNode = current.next
+                        # self.count -= 1
 
     def retrieve(self, key):
         # '''
@@ -109,15 +138,16 @@ class HashTable:
         # Fill this in.
         # '''
         # Get the index
-        index = self._hash(key)
-        # Point to the specific location
-        node = self.storage[index]
-        # Check if the node exists
-        if node is None:
-            return None
-        else:
-            # Return value if the node exists
-            return node.value
+        index = self._hash_mod(key)
+        target = self.storage[index]
+        while target is not None:
+            if target.key == key:
+
+                return target.value
+
+            else:
+                target = target.next
+        return None
 
     def resize(self):
         # '''
@@ -127,13 +157,28 @@ class HashTable:
         # Fill this in.
         # '''
         # double the capacity
-        self.capacity *= 2
+
+        self.capacity = self.capacity * 2
         # Set the new storage
         new_storage = [None] * self.capacity
-        # Iterate the old list and assign its value to the new one
-        for i in self.storage:
-            new_storage[i] = self.storage[i]
 
+        # Iterate the old list and assign its value to the new one
+        for i in range(len(self.storage)):
+            current = self.storage[i]
+
+            while current is not None:
+                # Create a new index
+                index = self._hash_mod(current.key)
+                # Check if index is taken
+                if new_storage[index] is None:
+                    # Assign value to the index in new storage
+                    new_storage[index] = LinkedPair(current.key, current.value)
+                else:
+                    # Assign value to the next since the index is occupied
+                    new_storage[index].next = LinkedPair(
+                        current.key, current.value)
+                current = current.next
+        self.storage = new_storage
 
 # if __name__ == "__main__":
 #     ht = HashTable(2)
@@ -148,6 +193,7 @@ class HashTable:
 #     print(ht.retrieve("line_1"))
 #     print(ht.retrieve("line_2"))
 #     print(ht.retrieve("line_3"))
+
 
 #     # Test resizing
 #     old_capacity = len(ht.storage)
